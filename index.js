d68f9f423c5301927b347e7af3d71840e665c80a
mishranitesh171 <niteshmishra171171171@gmail.com>
2023-12-25 19:27:54 +0530

    prodigy task1

---
+++ index.js
@@ -0,0 +1,15 @@
+let body = document.querySelector("body");
+body.addEventListener("load",()=>{
+    
+})
+
+
+const navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.top = "0";
+    
+}
+function hideMenu(){
+    navLinks.style.top = "-60vh";
+   
+}
